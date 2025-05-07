e42e56ae0ac72170e57e84437a524a2dab05d0d7
Gustavo Ronan <gustavoronn@gmail.com>
2025-05-07 20:12:51 -0300

    Listing invoices

---
+++ src/data.ts
@@ -0,0 +1,48 @@
+const invoices = [
+    {
+        name: "Santa Monica",
+        number: 1995,
+        amount: "$10,800",
+        due: "12/05/1995",
+    },
+    {
+        name: "Los Angeles",
+        number: 1996,
+        amount: "$9,450",
+        due: "01/10/1996",
+    },
+    {
+        name: "San Francisco",
+        number: 1997,
+        amount: "$12,200",
+        due: "03/15/1997",
+    },
+    {
+        name: "San Diego",
+        number: 1998,
+        amount: "$8,300",
+        due: "06/22/1998",
+    },
+    {
+        name: "Sacramento",
+        number: 1999,
+        amount: "$14,100",
+        due: "09/30/1999",
+    },
+    {
+        name: "Fresno",
+        number: 2000,
+        amount: "$7,950",
+        due: "11/18/2000",
+    },
+    {
+        name: "Long Beach",
+        number: 2001,
+        amount: "$10,500",
+        due: "01/05/2001",
+    },
+]
+
+export function getInvoices(){
+    return invoices;
+}
