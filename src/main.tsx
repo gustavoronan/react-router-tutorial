36a4c2d977e2ac2e18ee1e5d9a0d2863a3d4516c
Gustavo Ronan <gustavoronn@gmail.com>
2025-05-08 13:43:18 -0300

    add warning not found rote

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Expenses from './routes/Expenses/index.tsx'
 import Invoices from './routes/Invoices/index.tsx'
+import NotFound from './routes/NotFound/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,9 @@ createRoot(document.getElementById('root')!).render(
           <Route path = "/" element = {<App></App>}>
             <Route path = "/expenses" element = {<Expenses></Expenses>}></Route>
             <Route path = "/invoices" element = {<Invoices></Invoices>}></Route>
+            <Route path = "*" element = {
+              <NotFound></NotFound>
+            }></Route>
           </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/routes/NotFound/index.tsx
@@ -0,0 +1,10 @@
+
+export default function NotFound() {
+  return (
+    <>
+      <main style={{ padding: "1rem" }}>
+        <p>Não ha nada aqui!</p>
+      </main>
+    </>
+  );
+}
